e31dcd6c9eb21bd0a63824359991d908e88cda3d
Dylan Scott Mickelson <dylanmickelsons@gmail.com>
2026-08-31 16:46:08 -0700

    Update fast_crud_api.dart

--- lib/fast_crud_api.dart
+++ lib/fast_crud_api.dart
@@ -93,16 +93,10 @@ class APIServer {
     final Router app = Router();
 
     ///FAST CRUD DOCS UI Handler
-    // final flutterWebHandler = createStaticHandler(
-    //   'assets/web',
-    //   defaultDocument: 'index.html',
-    //   serveFilesOutsidePath: true,
-    // );
-    final staticHandler = serveStatic(
-  'assets/web',
-  rootPath: '/',
-  defaultDocument: 'index.html',
-);
+    final flutterWebHandler = createStaticHandler(
+      'assets/web',
+      defaultDocument: 'index.html',
+    );
 
     final InternetAddress address = InternetAddress.anyIPv4;
 
@@ -117,7 +111,7 @@ class APIServer {
       ], growable: true);
     }
 
-    app.get("/", staticHandler);
+    app.get("/", flutterWebHandler);
 
     app.get('/api/version', (Request request) async {
       return Response.ok(jsonEncode({"version": version ?? 1}));
