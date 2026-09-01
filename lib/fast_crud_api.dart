a6c0e8fe57cb8064a638ea45de0da9313fbeb676
Dylan Scott Mickelson <dylanmickelsons@gmail.com>
2026-08-31 17:31:35 -0700

    Make getPackageLibPath asynchronous
    Change resolvePackageUri to be asynchronous.

--- lib/fast_crud_api.dart
+++ lib/fast_crud_api.dart
@@ -95,7 +95,7 @@ class APIServer {
   final Router app = Router();
 
    String getPackageLibPath() {
-  final uri = Isolate.resolvePackageUri(
+  final uri = await Isolate.resolvePackageUri(
     Uri.parse('package:fast_crud_api/fast_crud_api.dart'),  
   );
   // uri is like: file:///path/to/.pub-cache/.../my_package/lib/some_file.dart
