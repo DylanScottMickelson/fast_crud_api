35f49631f9d6bab2cff369c1bdae02cd4a061486
Dylan Scott Mickelson <dylanmickelsons@gmail.com>
2026-08-31 18:25:19 -0700

    Add '/docs' endpoint for API documentation
    Updated API endpoint to serve documentation.

--- lib/fast_crud_api.dart
+++ lib/fast_crud_api.dart
@@ -142,7 +142,7 @@ final webDir = p.join(getPackageLibPath('fast_crud_api'), 'assets', 'web');
       ], growable: true);
     }
 
-    app.get("/", flutterWebHandler);
+    app.get("/docs", flutterWebHandler);
 
     app.get('/api/version', (Request request) async {
       return Response.ok(jsonEncode({"version": version ?? 1}));
